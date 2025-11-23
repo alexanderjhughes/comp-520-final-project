a94c3fff0b20f31ce5ccc495e5c1466b1735e360
Ryan <rjc5887@psu.edu>
2025-11-23 17:40:33 -0500

    preprocessing audio dataset

--- fma-genre-test.py
+++ fma-genre-test.py
@@ -1,26 +1,105 @@
+
 from datasets import load_dataset
 from torch import nn, randn
+import os
+import ffmpeg
+import torch
+from datasets import load_dataset, Audio
+from torch.utils.data import Dataset
+from transformers import ASTFeatureExtractor
+import io
+import numpy as np
+
+def ffmpeg_decode(audiobytes):
+	try:
+        #must use pcm_s16le acodec and then normalize so that waveform is expected size for feature extractor, error can be ignored just want output
+		output, error = (
+			ffmpeg.input("pipe:", format="mp3").output("pipe:", format="wav", acodec="pcm_s16le", ar = 16000, ac = 1).run(input=audiobytes, capture_stdout=True, quiet = True)
+		)
+	except Exception as e:
+		raise print("ffmpeg_decode Error:", type(e), e)
+
+	#transform into waveform into proper size and normalize to use in AST feature extractor
+    #transform pcm int16 into byte stream of int16, convert to float32, then divide |minimum int16 value| to normalize
+	waveform_encoding = np.frombuffer(output, np.int16).astype(np.float32) / 32768.0
+
+	#transform into pytorch tensor
+	waveform_tensor = torch.tensor(waveform_encoding).unsqueeze(0)
+
+	return waveform_tensor
+
+class SongsDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data = []
+        self.data_tensors = []
+        self.genre_labels = []
+        self.genre_labels_tensors = []
+        self.genres_uniq = ['Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 'International', 'Pop', 'Rock']
+
+        # load the dataset
+        dataset = load_dataset(data_dir)
+        train_data = dataset['train']
+        #disable default audio decoder
+        train_data = train_data.cast_column("audio", Audio(decode=False))
+
+        print(len(train_data))
+
+        # construct the AST feature extractor to pull features from the 
+        feature_extractor = ASTFeatureExtractor(num_mel_bins=16)
+
+        # build the audio and genre data by building the tensors from each audio item
+        # and storing them with genre labels
+        t = 0
+        for data_item in train_data:
+            t = t+1
+            #print(t)
+            audio_item = data_item["audio"]["bytes"]
+            genre_item = data_item['genre']
+
+            if audio_item is None:
+                print("Skipping empty audio item")
+                continue
+
+            try:
+            	waveform = ffmpeg_decode(audio_item)
+            	samp_rate = 16000
+            	audio_inputs = feature_extractor(waveform.squeeze().numpy(), sampling_rate=samp_rate, return_tensors="pt")
+            	self.data.append(audio_inputs)
+            	self.data_tensors.append(audio_inputs.input_values)
+            	self.genre_labels.append(genre_item)
+            except Exception as e: 
+            	print("SongsDataset Error:", e)
+
+            
+            #if t==100: break
+
+        for idx in range(len(self.genre_labels)):
+            temp_tensor = torch.tensor([self.genre_labels[idx]], dtype=torch.long)
+            self.genre_labels_tensors.append(temp_tensor)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        audio_data_item = self.data[idx]
+        genre_label = self.genre_labels[idx]
+        audio_data_tensor = self.data_tensors[idx]
+        genre_label_tensor = self.genre_labels_tensors[idx]
+
+        return genre_label_tensor, audio_data_tensor, genre_label, audio_data_item
 
 # Load the dataset
-dataset = load_dataset("rpmon/fma-genre-classification")
+dataset = SongsDataset("rpmon/fma-genre-classification")
+print(f"loaded {len(dataset)} items of data")
+print(f"example = {dataset[0]}")
 
-# Access training data
-train_data = dataset['train']
-
-# Get an audio sample and its genre
-audio = train_data[0]['audio']
-genre = train_data[0]['genre']
-print(audio)
-
+'''
 # Process with AST feature extractor
-from transformers import ASTFeatureExtractor
-feature_extractor = ASTFeatureExtractor(num_mel_bins=16)
-inputs = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"], return_tensors="pt")
-input_values = inputs.input_values
 #print(len(inputs))
 
-rnn = nn.RNN(16, 200, 2)
-h0 = randn(2, 1024, 200)
-output, hn = rnn(input_values,h0)
+rnn = torch.nn.RNN(16, 200, 2)
+h0 = torch.randn(2, 1024, 200)
+output, hn = rnn(torch.input_values,h0)
 
 print(output)
+'''
