3a80b2db7ed22ef4f5a3f7ac6465648414a679e2
Alexander Hughes <personal@alexanderhughes.dev>
2025-11-24 18:43:44 -0500

    feat: add len and getItem

--- FeatureTraining.py
+++ FeatureTraining.py
@@ -3,7 +3,7 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
-class SongsDataset(Dataset):
+class SongsFeatureTraining(Dataset):
     def __init__(self, data_dir):
         self.data = []
         self.data_tensors = []
@@ -16,9 +16,20 @@ class SongsDataset(Dataset):
         self.data_tensors = torch.load(f"{data_dir}_audio_features.pt")
         self.genre_labels_tensors = torch.load(f"{data_dir}_genre_labels.pt")
         self.genre_labels = np.load(f"{data_dir}_genre_labels.npy", allow_pickle=True).tolist()
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        audio_data_item = self.data[idx]
+        genre_label = self.genre_labels[idx]
+        audio_data_tensor = self.data_tensors[idx]
+        genre_label_tensor = self.genre_labels_tensors[idx]
+
+        return genre_label_tensor, audio_data_tensor, genre_label, audio_data_item
 
 def main():
-    dataset = SongsDataset("songsdata-november-24")
+    dataset = SongsFeatureTraining("songsdata-november-24")
     print(f"loaded {len(dataset)} items of data")
     print(f"example = {dataset[0]}")
 
