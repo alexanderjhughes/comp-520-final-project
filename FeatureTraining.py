8cbb2fe3d1fd0b2d2a9b3c282bfff705c38327df
Alexander Hughes <personal@alexanderhughes.dev>
2025-11-24 18:39:38 -0500

    feat: add feature training

---
+++ FeatureTraining.py
@@ -0,0 +1,26 @@
+from torch import nn, randn
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+
+class SongsDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data = []
+        self.data_tensors = []
+        self.genre_labels = []
+        self.genre_labels_tensors = []
+        self.genres_uniq = ['Electronic', 'Experimental', 'Folk', 'Hip-Hop', 'Instrumental', 'International', 'Pop', 'Rock']
+
+        # load the dataset from output files
+        self.data = torch.load(f"{data_dir}_data.pt")
+        self.data_tensors = torch.load(f"{data_dir}_audio_features.pt")
+        self.genre_labels_tensors = torch.load(f"{data_dir}_genre_labels.pt")
+        self.genre_labels = np.load(f"{data_dir}_genre_labels.npy", allow_pickle=True).tolist()
+
+def main():
+    dataset = SongsDataset("songsdata-november-24")
+    print(f"loaded {len(dataset)} items of data")
+    print(f"example = {dataset[0]}")
+
+if __name__ == "__main__":
+    main()
